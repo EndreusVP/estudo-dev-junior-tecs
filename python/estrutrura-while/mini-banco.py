ad1efdfdbc807dfc2f59dcc10c302f7050829d08
EndreusSre <endreus.valerio@gmail.com>
2025-09-11 04:05:02 -0300

    Update mini-banco.py

--- python/estrutrura-while/mini-banco.py
+++ python/estrutrura-while/mini-banco.py
@@ -1,26 +1,55 @@
 print("=====Banco Python=====")
 
+#saldo da conta 
+
+saldo = 0
+
 #funções das operações 
 
-def depositar(a):
-    deposito=0
-    a = deposito
-    return a
+def depositar(deposito):
 
-def sacar(a):
-    sacando=0
+    return saldo+deposito
+
+def sacar(saque):
+    return saldo - saque
+
+def consultar():
+    return saldo   
+
+#valor pra entrar no laço
+
+opcao = -1
+
+#laço pra operar as escolhas do usuario
+
+while opcao!=0:
+
+    #operações do banco
+
+    print("1 - Cosultar saldo")
+    print("2 - Depositar")
+    print("3 - Sacar")
+    print("0 - Sair")
+
+    #guardando operação
+
+    opcao=int(input("Escolha uma opcão: "))
+
+    #saindo do laço se opcao == 0
+
+    if opcao==0:
+        print("saindo...")
+        break
     
+    #condiçoes das operações
 
-#operações do banco
-
-print("1 - Cosultar saldo")
-print("2 - Depositar")
-print("3 - Sacar")
-print("0 - Sair")
-
-#guardando operação
-
-opcao=int(input("Escolha uma opcão: "))
-
+    if opcao==1:
+        print("Seu saldo: ", consultar())
+    elif opcao==2:
+        depositando = int(input("digite o valor do deposito: "))
+        depositar(depositando)
+    elif opcao==3:
+        #saque = int(input("digite o valor q quer sacar"))
+        sacar()
 
 
