0af1e06db2a10d9a66d5b372782f7cf5f8ffd270
EndreusSre <endreus.valerio@gmail.com>
2025-09-11 03:17:27 -0300

    Update mini-banco.py

--- python/estrutrura-while/mini-banco.py
+++ python/estrutrura-while/mini-banco.py
@@ -1,3 +1,26 @@
 print("=====Banco Python=====")
 
+#funções das operações 
+
+def depositar(a):
+    deposito=0
+    a = deposito
+    return a
+
+def sacar(a):
+    sacando=0
+    
+
 #operações do banco
+
+print("1 - Cosultar saldo")
+print("2 - Depositar")
+print("3 - Sacar")
+print("0 - Sair")
+
+#guardando operação
+
+opcao=int(input("Escolha uma opcão: "))
+
+
+
