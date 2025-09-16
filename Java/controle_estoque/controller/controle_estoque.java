fb0f64df18473913ca40999f814b719a35a1300a
ENDREUS VALÉRIO <endreus.parente007@gmail.com>
2025-09-16 18:25:43 -0300

    commit

--- Java/controle_estoque/controller/controle_estoque.java
+++ Java/controle_estoque/controller/controle_estoque.java
@@ -1,5 +1,23 @@
 package Java.controle_estoque.controller;
 
+import java.util.Scanner;
+
 public class controle_estoque {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o nome do produto");
+        String nome_produto_estoque = entrada.nextLine();
+
+        System.out.println("digite a quantidade do produto");
+        int quantidade_estoque = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("=====MENU=====");
+
+        System.out.println("1 - adicionar item");
+        System.out.println("2 - ");
+
+        entrada.close();
+    }
 }
