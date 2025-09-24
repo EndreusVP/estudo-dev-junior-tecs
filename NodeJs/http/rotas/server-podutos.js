b6b37d4d5e84de078b6fbd8b6d50a00536b3a0e9
EndreusSre <endreus.valerio@gmail.com>
2025-09-24 14:13:35 -0300

    commit

---
+++ NodeJs/http/rotas/server-podutos.js
@@ -0,0 +1,22 @@
+const http = require('http')
+let lista_produtos = ["maçã", "uva", "pera"]
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.writeHead(200, {"Content-Type" : "text/html"})
+        res.end("<h1>Pagina inicial</h1><p>Voce esta na Pagina Inicial</p>")
+    } else if(req.url === "/produtos"){
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        res.end(`<h1>Meus Produtos</h1>${lista_produtos}`)
+    } else if(req.url === "/servicos"){
+        res.writeHead(200, {'Content-type' : 'text/html'})
+        res.end("<h1>Serviços</h1><p>Esses são meus serviços: </p>")
+    } else {
+        res.writeHead(404, {'Content-type' : 'text/html'})
+        res.end("<h1>ERR 404</h1><p>Erro ao encontrar a pagina</p>")
+    }
+})
+
+server.listen(3000, () => {
+    console.log("esta usando o http://localhost:3000")
+})
